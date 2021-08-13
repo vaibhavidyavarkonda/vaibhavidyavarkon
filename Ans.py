719009e328c0bc5d5910507cc90f018a7d001a25
vaibhavidyavarkonda <88705439+vaibhavidyavarkonda@users.noreply.github.com>
2021-08-13 13:31:13 +0530

    Add files via upload

---
+++ Ans.py
@@ -0,0 +1,24 @@
+ Which is the correct operator for power(xy)? :- x**y
+
+------------------------------------------------------------------------
+
+    Which one of these is floor division?:- //
+
+---------------------------------------------------------------------
+
+    What is the order of precedence in python? :- Parentheses,Exponential,Multiplication,Division,Addition,Subtraction
+
+-----------------------------------------------------------------------------
+
+    What is the answer to this expression, 22 % 3 is? :- 1
+
+-----------------------------------------------------------------------------------
+
+    Mathematical operations can be performed on a string. :- False
+
+----------------------------------------------------------------------
+
+    what is meant by Operator in Python :- They are the special symbols in python 
+
+
+     How many types of Operators in Python : 7  Operators are there in Python

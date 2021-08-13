66e95e05c10974b38920f7bea691b0a93afe0788
vaibhavidyavarkonda <88705439+vaibhavidyavarkonda@users.noreply.github.com>
2021-08-13 13:29:26 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,43 @@
+#Arithmetic operators
+x=8
+y=6
+z=7
+print("\n x+y= ",x+y)
+print("\n x-z= ",x-z)
+print("\n z*y= ",z*y)
+print("\n x/y= ",x/y)
+print("\n z**y= ",z**y)
+
+#Comparison  operators
+p=4
+f=6
+print("\n p>f=",p>f)
+print("\n p<f=",p<f)
+print("\n p!=f",p!=f)
+
+#Logical operators
+a=True
+b=False
+print("\na and b is",a and b)
+print("\na or b is",a or b)
+print("\nnot b is",not b)
+
+#Identity operators
+x1=3
+x2="hey"
+x3=[3,4,5,6]
+y1=3
+y2="hey"
+y3=[3,4,5,6]
+print("\n x1 is not y2",x1 is not y2)
+print("\n x1 is not y1",x1 is not y1)
+print("\n x2 is y2",x1 is y2)
+print("\n x3 is  y2",x2 is  y2)
+
+#Membership operators
+m1="Hey"
+m2={1:'a',2:'b'}
+print("\n" 'H' in m1)
+print("\n "'hello' not  in m1)
+print(1 in m2)
+print('a' in m2) 

378edda6dab53bb15ab41aa5c8fef3e3e1d56f4e
vaibhavidyavarkonda <88705439+vaibhavidyavarkonda@users.noreply.github.com>
2021-08-14 13:33:05 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,13 @@
+#Write a program to manipulate Tuple data
+
+B=("c","c++","java","python",1,2,3,4)
+
+print("Tuple B:",B)
+
+print("Tuple B: 2 to 5",B[2:6])
+
+print("Tuple B in reverse:",B[::-1])
+
+print("Count of Apple is :",B.count('c'))
+
+print("Index of Apple",B.index('c'))
